f945a9946b3eef2e2670059eebb7c0936a1adf18
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-08 13:17:19 +0200

    finishing bootstraping

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -19,16 +19,12 @@ pub fn bootstrap(rawpkg: &String, bootstrap_path: &str) -> Result<()> {
     //let pkg_name = rawpkg.split_once(".raw").map(|(name, _)| name).unwrap_or(rawpkg);
     File::create("/var/cache/tmp.raw").context("Not running as root, aborting")?;
     fs::remove_file("/var/cache/tmp.raw").unwrap();
-    if Path::new("/tmp/conflict").exists() {
-        fs::remove_file("/tmp/conflict").unwrap();
-    } else {
-        conflict(&rawpkg);
-    }
     let pkg = rawpkg.split_once('.').map(|(pkg, _)| pkg).unwrap();
-    fs::create_dir(format!("/var/lib/pkg/DB/{}", pkg)).unwrap();
-    println!("Copying {} to /var/lib/pkg/DB/{}/{}", rawpkg, pkg, rawpkg);
-    fs::copy(rawpkg, format!("/var/lib/pkg/DB/{}/{}", pkg, rawpkg)).unwrap();
-    env::set_current_dir(format!("/var/lib/pkg/DB/{}", pkg)).unwrap();
+    fs::create_dir(format!("{}/var/lib/pkg/DB/", bootstrap_path)).unwrap();
+    fs::create_dir(format!("{}/var/lib/pkg/DB/{}", bootstrap_path, pkg)).unwrap();
+    println!("Copying {} to /var/lib/pkg/DB/{}/{} in bootstrap directory", rawpkg, pkg, rawpkg);
+    fs::copy(rawpkg, format!("{}/var/lib/pkg/DB/{}/{}", bootstrap_path, pkg, rawpkg)).unwrap();
+    env::set_current_dir(format!("{}/var/lib/pkg/DB/{}", bootstrap_path, pkg)).unwrap();
     if rawpkg.ends_with(".tar.gz") || rawpkg.ends_with(".tgz") {
         let file = fs::File::open(rawpkg).unwrap();
         let mut archive = Archive::new(GzDecoder::new(file));
@@ -44,76 +40,14 @@ pub fn bootstrap(rawpkg: &String, bootstrap_path: &str) -> Result<()> {
         content_only: false,
         ..Default::default()
     };
-    if Path::new(&format!("{}.pre-install", pkg)).exists() {
-        let pre_install = format!("chmod u+x {}.pre-install && ./{}.pre-install", pkg, pkg);
-        println!("Starting pre-installation.");
-        Command::new("bash")
-        .args(["-c", &pre_install])
-        .status()
-        .unwrap();
-        fs::remove_file(format!("{}.pre-install", pkg));
-    } else {
-        println!("No pre-installation required");
-    }
     copy_recursive(Path::new("."), Path::new(bootstrap_path), &opts).unwrap();
-    println!("running ldconfig.....");
-    Command::new("bash")
-    .args(["-c", "ldconfig"])
-    .status()
-    .unwrap();
-    if Path::new(&format!("{}.post-install", pkg)).exists() {
-        let post_install = format!("chmod u+x {}.post-install && ./{}.post-install", pkg, pkg);
-        println!("Starting post-installation.");
-        Command::new("bash")
-        .args(["-c", &post_install])
-        .status()
-        .unwrap();
-        fs::remove_file(format!("{}.post-install", pkg));
-    } else {
-        println!("No post-installation required");
-    }
-    fs::remove_dir_all(format!("/var/lib/pkg/DB/{}", pkg)).unwrap();
-    fs::create_dir(format!("/var/lib/pkg/DB/{}", pkg)).unwrap();
-    fs::copy("/META", format!("/var/lib/pkg/DB/{}/META", pkg)).unwrap();
-    fs::copy(format!("/{}.footprint", pkg), format!("/var/lib/pkg/DB/{}/files", pkg)).unwrap();
-    fs::remove_file("/META").unwrap();
-    fs::remove_file(format!("/{}.footprint", pkg)).unwrap();
-    fs::remove_file(format!("/{}", rawpkg)).unwrap();
-    if Path::new(&format!("/{}.pre-install", pkg)).exists() {
-        fs::remove_file(format!("/{}.pre-install", pkg)).unwrap();
-    }
-    if Path::new(&format!("/{}.post-install", pkg)).exists() {
-        fs::remove_file(format!("/{}.post-install", pkg)).unwrap();
-    }
-    let content = fs::read_to_string(format!("/var/lib/pkg/DB/{}/files", pkg)).unwrap();
+    fs::remove_dir_all(format!("{}/var/lib/pkg/DB/{}", bootstrap_path, pkg)).unwrap();
+    fs::create_dir(format!("{}/var/lib/pkg/DB/{}", bootstrap_path, pkg)).unwrap();
+    fs::copy(format!("{}/META", bootstrap_path), format!("{}/var/lib/pkg/DB/{}/META", bootstrap_path, pkg)).unwrap();
+    fs::copy(format!("{}/{}.footprint", bootstrap_path, pkg), format!("{}/var/lib/pkg/DB/{}/files", bootstrap_path, pkg)).unwrap();
+    fs::remove_file(format!("{}/META", bootstrap_path)).unwrap();
+    fs::remove_file(format!("{}/{}.footprint", bootstrap_path, pkg)).unwrap();
+    fs::remove_file(format!("{}/{}", bootstrap_path, rawpkg)).unwrap();
     //let content = line.lines();
-    if content.contains(".desktop") {
-        if Path::new("/usr/bin/gtk-update-icon-cache").exists() {
-            Command::new("bash")
-                // glib-compile-schemas /usr/share/glib-2.0/schemas
-            .args(["-c", "glib-compile-schemas /usr/share/glib-2.0/schemas"])
-            .status()
-            .unwrap();
-            println!("Compiling gschemas")
-        }
-        if Path::new("/usr/bin/gtk-update-icon-cache").exists() {
-            for entry in WalkDir::new("/usr/share/icons").max_depth(1).min_depth(1) {
-                let foot = entry.unwrap().path().display().to_string();
-                if file_type(&foot) == false {
-                    env::set_current_dir(bootstrap_path).unwrap();
-                    env::set_current_dir(&foot).unwrap();
-                    println!("{}", foot);
-                    let directory = format!("/usr/bin/gtk-update-icon-cache -f -t {}", foot);
-                    Command::new("bash")
-                    .args(["-c", &directory])
-                    .status()
-                    .unwrap();
-                    println!("Updating icon cache");
-                }
-                
-            }
-        }
-    }
-
     Ok(())
 }
