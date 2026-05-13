180498ad79050d349cabd46ff1aec8fec98928b4
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-14 00:42:40 +0200

    changing from red to green + license

--- src/remove_cache.rs
+++ src/remove_cache.rs
@@ -1,3 +1,20 @@
+// Raw is a simple package manager written in rust, it aims to be compatible with the Pkgfiles written that works with pkgmk from pkgutils/cards
+//    Copyright (C) 2026  Alexis/Delta-Azura
+
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License, or
+//    (at your option) any later version.
+
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License along
+//    with this program; if not, write to the Free Software Foundation, Inc.,
+//    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 use anyhow::{Result};
 use std::fs;
 use anyhow::Context;
@@ -6,7 +23,6 @@ use crate::file_type::file_type;
 const RESET: &str = "\x1b[0m";
 const GREEN: &str = "\x1b[0;32m";
 const YELLOW: &str = "\x1b[33m";
-const RED: &str = "\x1b[1;31m";
 
 
 pub fn remove_cache() -> Result<()> {
@@ -20,7 +36,7 @@ pub fn remove_cache() -> Result<()> {
         if file_type(&full_path) == true {
             println!("{}", i);
             fs::remove_file(&full_path)?;
-            println!("{}Successfully removed {}{}", RED, i, RESET);
+            println!("{}Successfully removed {}{}", GREEN, i, RESET);
         } else {
             println!("{}{} is not a package to remove, continue....{}", YELLOW, i, RESET);
             continue
