9146b59cde47fc64562f521941845d337cb34ede
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-16 00:24:27 +0200

    test

--- src/bootstrap.rs
+++ src/bootstrap.rs
@@ -39,7 +39,7 @@ pub fn bootstrap(rawpkg: &String, bootstrap_path: &str) -> Result<()> {
     File::create("/var/cache/tmp.raw").context("Not running as root, aborting")?;
     fs::remove_file("/var/cache/tmp.raw").unwrap();
     let pkg = rawpkg.split_once('.').map(|(pkg, _)| pkg).unwrap();
-    fs::create_dir_all(format!("{}/var/lib/pkg/DB/{}", bootstrap_path, pkg)).unwrap();
+    fs::create_dir_all(format!("{}/var/lib/pkg/DB/{}/{}", bootstrap_path, pkg, rawpkg)).unwrap();
     println!("Copying {} to /var/lib/pkg/DB/{}/{} in bootstrap directory", rawpkg, pkg, rawpkg);
     fs::copy(rawpkg, format!("{}/var/lib/pkg/DB/{}/{}", bootstrap_path, pkg, rawpkg)).unwrap();
     env::set_current_dir(format!("{}/var/lib/pkg/DB/{}", bootstrap_path, pkg)).unwrap();
