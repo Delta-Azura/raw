b0d562e96ba45685ea09983836d92a15de01a94a
Delta-Azura <alexis.prev.delta07@gmail.com>
2026-05-30 14:23:58 +0200

    adding automatic installation in build

--- src/build.rs
+++ src/build.rs
@@ -20,6 +20,8 @@ use anyhow::{Result, Context};
 use crate::package;
 use std::fs;
 use crate::getconf;
+use std::process::Command;
+use std::path::Path;
 
 pub fn build(pkg: &str) -> Result<()> {
     getconf().unwrap();
@@ -27,7 +29,29 @@ pub fn build(pkg: &str) -> Result<()> {
     let path = fs::read_to_string("index.raw").context("index.raw doesn't exist, please run raw index")?;
     let path = path.lines().find(|l| l.contains(&format!("{}/", pkg))).context("This package doesn't exists on the index")?.split_once("/Pkgfile").map(|(path, _)| path).unwrap().to_string();
     println!("{}", path);
-    env::set_current_dir(path)?;
-    package(None)?;
+    for entry in fs::read_dir(&path)? {
+        let entry = entry?;
+        if entry.file_name().to_string_lossy().contains(".raw.") {
+            env::set_current_dir(&path)?;
+            println!("current path is {}", path);
+            let potential_package = entry.file_name().to_string_lossy().to_string();
+            let pkgver =  entry.file_name().to_string_lossy().split_once('.').map(|(_, pkgver)| pkgver).unwrap().split_once("#").map(|(pkgver, _)| pkgver).unwrap().to_string();
+            let pkgrel = entry.file_name().to_string_lossy().split_once('#').map(|(_, pkgver)| pkgver).unwrap().split_once(".").map(|(pkgver, _)| pkgver).unwrap().to_string();
+            if !Path::new("Pkgfile").exists() {
+                Command::new("sudo").args(["raw", "install", &potential_package]).status()?;
+            } else {
+                let pkgfile_comp = fs::read_to_string("Pkgfile")?;
+                let pkgverfile = pkgfile_comp.lines().find(|l| l.starts_with("version=")).context("No line found")?.split_once("version=").map(|(_, version)| version).context("no pkg version mentionned in pkgfile")?;
+                let pkgrelfile = pkgfile_comp.lines().find(|l| l.starts_with("release=")).context("No line found")?.split_once("release=").map(|(_, version)| version).context("no pkg release mentionned in pkgfile")?;
+                if pkgver == pkgverfile || pkgrel == pkgrelfile {
+                    Command::new("sudo").args(["raw", "install", &potential_package]).status()?;
+                } else {
+                    package(None)?;
+                    Command::new("sudo").args(["raw", "install", &potential_package]).status()?;
+                }
+            }
+
+        }
+    }
     Ok(())
 }
